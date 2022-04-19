b5bd1488f1439906b29853b75d946f1df5235118
juzerBasia <basiawielek@gmail.com>
2022-04-19 12:34:55 +0200

    final invoiceTest implemented

--- src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
@@ -3,40 +3,40 @@ package pl.edu.agh.mwo.invoice.product;
 import java.math.BigDecimal;
 
 public abstract class Product {
-    private final String name;
-    private final BigDecimal price;
-    private final BigDecimal taxPercent;
-    private final BigDecimal excise;
+	private final String name;
+	private final BigDecimal price;
+	private final BigDecimal taxPercent;
+	private final BigDecimal excise;
 
-    protected Product(String name, BigDecimal price, BigDecimal tax, BigDecimal excise) {
-        if (name == null || name.equals("") || price == null
-                || tax == null || tax.compareTo(new BigDecimal(0)) < 0
-                || price.compareTo(new BigDecimal(0)) < 0) {
-            throw new IllegalArgumentException();
-        }
-        this.name = name;
-        this.price = price;
-        this.taxPercent = tax;
-        this.excise = excise;
-    }
+	protected Product(String name, BigDecimal price, BigDecimal tax, BigDecimal excise) {
+		if (name == null || name.equals("") || price == null
+				|| tax == null || tax.compareTo(new BigDecimal(0)) < 0
+				|| price.compareTo(new BigDecimal(0)) < 0) {
+			throw new IllegalArgumentException();
+		}
+		this.name = name;
+		this.price = price;
+		this.taxPercent = tax;
+		this.excise = excise;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public BigDecimal getPrice() {
-        return price;
-    }
+	public BigDecimal getPrice() {
+		return price;
+	}
 
-    public BigDecimal getTaxPercent() {
-        return taxPercent;
-    }
+	public BigDecimal getTaxPercent() {
+		return taxPercent;
+	}
 
-    public BigDecimal getPriceWithTax() {
-        return price.multiply(taxPercent).add(price);
-    }
+	public BigDecimal getPriceWithTax() {
+		return price.multiply(taxPercent).add(price);
+	}
 
-    public BigDecimal getExcise() {
-        return excise;
-    }
+	public BigDecimal getExcise() {
+		return excise;
+	}
 }
